d03aefa801eb59e25ea2312e092cdfbfc23c96e1
nazavod <91217742+nazavod777@users.noreply.github.com>
2024-11-21 16:45:31 +0300

    Update main.go

--- main.go
+++ main.go
@@ -41,6 +41,16 @@ func startTasks(userAction int,
 
 	if userAction == 1 {
 		fmt.Println()
+		fmt.Printf("Threads: ")
+		threads, err := strconv.Atoi(inputUser())
+
+		if err != nil {
+			log.Panicf("Wrong Threads Number: %s", err)
+		}
+
+		global.Semaphore = make(chan struct{}, threads)
+		fmt.Println()
+
 		for i := 0; i < len(accountsList); i++ {
 			account := accountsList[i]
 			proxy := proxyList[i]
@@ -87,6 +97,16 @@ func startTasks(userAction int,
 		var mu sync.Mutex
 
 		fmt.Println()
+		fmt.Printf("Threads: ")
+		threads, err := strconv.Atoi(inputUser())
+
+		if err != nil {
+			log.Panicf("Wrong Threads Number: %s", err)
+		}
+
+		global.Semaphore = make(chan struct{}, threads)
+		fmt.Println()
+
 		for i := 0; i < len(accountsList); i++ {
 			account := accountsList[i]
 			proxy := proxyList[i]
